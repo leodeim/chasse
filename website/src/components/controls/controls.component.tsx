605f9c433a503c6ae9fcd0c6482bcd4813b93184
Leonidas Deimantas <leonidas.deimantas@gmail.com>
2022-10-02 18:36:21 +0300

    ui: small ui fix

--- website/src/components/controls/controls.component.tsx
+++ website/src/components/controls/controls.component.tsx
@@ -39,7 +39,7 @@ export default function Controls() {
     }
     
     return (
-        <div className="flex sm:flex-col justify-center ml-4 text-green">
+        <div className="flex sm:flex-col justify-center sm:ml-4 text-green">
             <div className="flex sm:flex-col mt-4 pb-2 sm:mt-0 sm:pt-4 sm:pb-4 bg-yellow rounded-full">
                 <button className="pt-2 pl-4 pr-4 sm:pl-2 sm:pr-2" onClick={() => goBack()}>
                     <BackIcon />
--- website/src/components/menu/menu.component.tsx
+++ website/src/components/menu/menu.component.tsx
@@ -3,7 +3,7 @@ import { HomeIcon, ShareIcon } from '../../utilities/icons.utility'
 
 export default function Menu() {
     return (
-        <div className="flex sm:flex-col justify-center mr-4 text-yellow">
+        <div className="flex sm:flex-col justify-center sm:mr-4 text-yellow">
             <div className="flex sm:flex-col mb-4 pt-2 sm:mb-0 sm:pt-4 sm:pb-4 bg-darkGreen rounded-full">
                 <Link to="/" className="pb-2 pl-4 pr-4 sm:pl-2 sm:pr-2 sm:pt-2" onClick={() => { }}>
                     <HomeIcon />
