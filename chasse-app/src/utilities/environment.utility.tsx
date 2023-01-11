02bf6056aa28e02e128b4735a8963ea8cb81ec3b
Leonidas Deimantas <leonidas.deimantas@gmail.com>
2023-01-11 15:51:17 +0200

    api and ws url move to env variables

--- chasse-app/src/utilities/environment.utility.tsx
+++ chasse-app/src/utilities/environment.utility.tsx
@@ -1,17 +1,17 @@
 export function getApiUrl(): string {
-    const DEV_MODE = process.env.REACT_APP_DEV_MODE;
-    if (DEV_MODE !== undefined) {
+    const CUSTOM_URL = process.env.REACT_APP_API_URL;
+    if (CUSTOM_URL === undefined) {
         return "http://localhost:8085/"
     }
-
-    return "https://chasse.fun/"
+    
+    return CUSTOM_URL
 }
 
 export function getWebsocketUrl(): string {
-    const DEV_MODE = process.env.REACT_APP_DEV_MODE;
-    if (DEV_MODE !== undefined) {
+    const CUSTOM_URL = process.env.REACT_APP_WS_URL;
+    if (CUSTOM_URL === undefined) {
         return "ws://localhost:8085/"
     }
 
-    return "wss://chasse.fun/"
+    return CUSTOM_URL
 }
